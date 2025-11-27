0d6a2e62dddd16b6cc33114f0df7a9e2d94a9e96
nurmaulidinaa2007-beep <nurmaulidinaa2007@gmail.com>
2025-11-27 08:21:20 +0800

    Add triangle area calculation in Segitiga.py

---
+++ Segitiga.py
@@ -0,0 +1,8 @@
+#nama : nur maulidina 
+#kelas sistem informasi B
+#nim 
+
+alas_segitiga = int(input("masukan alas segitiga :"))
+tinggi_segitiga = int(input("masukan tinggi segitiga : "))
+luas = alas_segitiga * tinggi_segitiga
+print(f"alas : {alas_segitiga} x {tinggi_segitiga} : {luas}")
