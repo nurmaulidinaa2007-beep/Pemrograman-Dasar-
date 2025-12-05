550cb57df01b8f7ef10814b771edf430d1044766
nurmaulidinaa2007-beep <nurmaulidinaa2007@gmail.com>
2025-12-05 17:21:40 +0800

    Create segiempat.py

---
+++ menghitung luas/segiempat.py
@@ -0,0 +1,10 @@
+# Nama : nurmaulidina
+# Nim : D0425317
+# luas persegi
+
+s = int(input("masukkan sisi pertama: "))
+s2 = int(input("Masukkan sisi kedua: "))
+
+luas = int(s * s2 ) 
+
+print('luas persegi adalah: ' , luas)
